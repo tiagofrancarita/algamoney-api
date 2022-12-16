b13afa74c76b7369441d103da8f335fc7d8c4960
Tiago França <tiagofranca.rita@gmail.com>
2022-12-16 17:30:18 -0300

    Initial commit

---
+++ src/main/java/com/franca/moneyalgaapi/MoneyalgaapiApplication.java
@@ -0,0 +1,13 @@
+package com.franca.moneyalgaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyalgaapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyalgaapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/franca/moneyalgaapi/MoneyalgaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.moneyalgaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyalgaapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
