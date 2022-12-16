69873286868e9d57693dcad328e240ece423b152
Tiago França <tiagofranca.rita@gmail.com>
2022-12-16 20:48:10 -0300

    Criação dos paoctes de repositorio,modelo, security e resource. Criação do metodo listar categoria.

---
+++ src/main/java/com/franca/moneyalgaapi/resource/CategoriaResource.java
@@ -0,0 +1,31 @@
+package com.franca.moneyalgaapi.resource;
+
+import com.franca.moneyalgaapi.model.Categoria;
+import com.franca.moneyalgaapi.rapository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    public CategoriaResource(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @GetMapping("/listartodas")
+    public List<Categoria> listar(){
+
+        return categoriaRepository.findAll();
+
+    }
+
+}
---
+++ src/main/java/com/franca/moneyalgaapi/security/WebConfigSecurity.java
@@ -0,0 +1,24 @@
+package com.franca.moneyalgaapi.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpSessionListener;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.GET, "/categorias/listartodas")
+                .antMatchers(HttpMethod.POST, "/categorias/cadastrarcategoria");
+    }
+
+}
