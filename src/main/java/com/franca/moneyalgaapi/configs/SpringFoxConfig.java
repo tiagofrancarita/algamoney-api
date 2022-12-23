4818675549061990d77dd2f62e07163ebdf8ca20
Tiago França <tiagofranca.rita@gmail.com>
2022-12-22 23:03:16 -0300

    Documentação API - SWAGGER

--- src/main/java/com/franca/moneyalgaapi/configs/SpringFoxConfig.java
+++ src/main/java/com/franca/moneyalgaapi/configs/SpringFoxConfig.java
@@ -4,10 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {
 
     @Bean
@@ -17,6 +24,22 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.franca.moneyalgaapi.resource"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "API - FINANÇA",
+                "API para o geranciamento de finanças pessoais",
+                "1.0.0",
+                "Termos e serviços",
+                new Contact("Tiago França","https://github.com/tiagofrancarita",
+                        "tiagofranca.rita@gmail.com"),
+                "Apache Licence",
+                "https://www.apache.org/licesen.html",new ArrayList<VendorExtension>());
+
+        return apiInfo;
     }
 }
